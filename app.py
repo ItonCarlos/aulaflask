91120fc24315e0042ed86efe4f99eb5f5de5ac25
Iton Carlos Sangaletti <itoncsangaletti@gmail.com>
2024-07-30 22:07:46 -0300

    inicio da criação da classe livro

--- app.py
+++ app.py
@@ -3,9 +3,23 @@ import pandas as pd
 
 app = Flask(__name__)
 
+class Livro:
+    def __init__(self, titulo, autor, categoria, ano, editora):
+        self.titulo = titulo
+        self.autor = autor
+        self.categoria = categoria
+        self.ano = ano
+        self.editora = editora
+        self.ativo = False
+       
+
 @app.route('/inicio')
 def ola():
-    lista = ['O Senhor dos Anéis', 'Dom Casmurro', 'O Alquimista','O Livro']
+    livro1 = Livro("O Senhor dos Anéis", "J.R.R. Tolkien", "Fantasia", 1954, "HarperCollins")
+    livro2 = Livro("Dom Casmurro", "Machado de Assis", "Romance", 1899, "Martin Claret")
+    livro3 = Livro("O Alquimista", "Paulo Coelho", "Autoajuda", 1988, "Rocco")
+    #lista = ["O Senhor dos Anéis", "Dom Casmurro", "O Alquimista"]
+    lista = [livro1,livro2, livro3]
     return render_template('lista.html', titulo='Listagem de Livros - SENAI',lista_de_livros=lista)
 
 @app.route('/curriculo')
